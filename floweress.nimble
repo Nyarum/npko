4c9db402d4a5a1258adc1cf20f91c6a2a4d4f471
Grigorii Eleskin <nyarumilian@gmail.com>
2024-12-16 16:21:00 +0400

    feat: init

---
+++ floweress.nimble
@@ -0,0 +1,15 @@
+# Package
+
+version       = "0.1.0"
+author        = "Grigorii Eleskin"
+description   = "A new awesome nimble package"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["floweress"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.0"
+
+requires "weave >= 0.4.10"
---
+++ src/floweress.nim
@@ -0,0 +1,43 @@
+import  strformat, strutils, net, asyncdispatch, asyncnet
+
+proc handleClient(client: AsyncSocket) {.async.} =
+  defer:
+    client.close()
+    echo "Client disconnected."
+    
+  echo "Client connected: ", client.getPeerAddr()
+
+  while not client.isClosed:
+    try:
+      let buf = await client.recv(1024)
+      echo buf.len
+      if buf.len == 0:
+        break  # Client disconnected
+
+      let data = buf.strip()
+      echo "Received from client: ", data
+      await client.send("You said: " & data)  # Echo back the data
+    except OSError:
+      echo "os error"
+      break  # Handle client disconnection or errors
+
+proc startServer(port: Port) {.async.} =
+  var server = newAsyncSocket(buffered=false)
+  server.setSockOpt(OptReuseAddr, true)
+  server.bindAddr(port)
+  echo fmt"Started tcp server... {port}"
+  server.listen()
+
+  while true:
+    try:
+      let client = await server.accept()
+      echo "..Got connection "
+
+      asyncCheck handleClient(client)
+    except:
+      echo "closed server"
+      break
+
+when isMainModule:
+  asyncCheck startServer(8085.Port)
+  runForever()
